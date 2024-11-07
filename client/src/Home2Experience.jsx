b953e17de28615aba19001ab603e41a9f4b3fa66
moon-rhino <beatrhino@gmail.com>
2024-11-06 21:46:10 -0800

    add another star in home2

--- client/src/Home2Experience.jsx
+++ client/src/Home2Experience.jsx
@@ -52,5 +52,6 @@ export default function Home2Experience() {
             <Constellation omitFactor={2} position={[5,-1,-6]} rotation={[-Math.PI/2, 0, Math.PI/4]} scale={1.5} modelPath={'models/star.glb'}/>
 
         {/* </Float> */}
+            <Constellation omitFactor={2} position={[-6.1,-1,3]} rotation={[-Math.PI/2, 0, Math.PI/12]} scale={1.2} modelPath={'models/star.glb'}/>
     </group>
 }
